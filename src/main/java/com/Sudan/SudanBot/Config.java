b8c55b2bfb33e7f7e25a4c56a34d43aac2e2a9c4
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-09-08 12:35:25 +1000

    Updated Config to replace spaces with underscores

--- src/main/java/com/Sudan/SudanBot/Config.java
+++ src/main/java/com/Sudan/SudanBot/Config.java
@@ -5,9 +5,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class Config {
     private static final Dotenv dotenv = Dotenv.load();
     public static String get(String key) {
-        return dotenv.get(key.toUpperCase());
+        return dotenv.get(key.replace(' ', '_').toUpperCase());
     }
     public static String get(String key, String value) {
-        return dotenv.get(key.toUpperCase(), value);
+        return dotenv.get(key.replace(' ', '_').toUpperCase(), value);
     }
 }
