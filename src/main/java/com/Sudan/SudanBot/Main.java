93b48f3cb69aca2e25912205204607f28b2ecd77
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2021-09-08 12:04:16 +1000

    Added option to register commands in an individual guild for debugging

--- src/main/java/com/Sudan/SudanBot/Main.java
+++ src/main/java/com/Sudan/SudanBot/Main.java
@@ -2,6 +2,7 @@ package com.Sudan.SudanBot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.Command;
 
 import javax.security.auth.login.LoginException;
@@ -11,14 +12,38 @@ public class Main {
         JDA api = JDABuilder.createDefault(Config.get("token")).build();
         CommandHandler commands = new CommandHandler();
         api.awaitReady();
-        if (args.length >= 1 && args[0].equals("register")) {
-            for (Command command : api.retrieveCommands().complete()) {
-                command.delete().queue();
+        if (args.length >= 1) {
+            switch (args[0]) {
+                case "register" -> {
+                    for (Command command : api.retrieveCommands().complete()) {
+                        command.delete().queue();
+                    }
+                    for (ICommand command : commands.commands) {
+                        api.upsertCommand(command.command()).queue();
+                    }
+                    api.shutdown();
+                }
+                case "debug" -> {
+                    Guild guild = api.getGuildById(args[1]);
+                    assert guild != null;
+                    for (Command command : guild.retrieveCommands().complete()) {
+                        command.delete().queue();
+                    }
+                    for (ICommand command : commands.commands) {
+                        guild.upsertCommand(command.command()).queue();
+                    }
+                    api.shutdown();
+                }
+                case "undebug" -> {
+                    Guild guild = api.getGuildById(args[1]);
+                    assert guild != null;
+                    for (Command command : guild.retrieveCommands().complete()) {
+                        command.delete().queue();
+                    }
+                    api.shutdown();
+                }
+                default -> throw new IllegalArgumentException("Invalid command");
             }
-            for (ICommand command : commands.commands) {
-                api.upsertCommand(command.command()).queue();
-            }
-            api.shutdown();
         } else {
             api.addEventListener(commands);
         }
